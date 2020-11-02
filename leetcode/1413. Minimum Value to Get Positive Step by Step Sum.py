1610880a77b2848fc29bd5027d073601361be421
Alexander Radchenko <48209253+radchenko511@users.noreply.github.com>
2020-11-02 12:31:15 -0800

    Add 1413

--- leetcode/1413. Minimum Value to Get Positive Step by Step Sum.py
+++ leetcode/1413. Minimum Value to Get Positive Step by Step Sum.py
@@ -4,7 +4,7 @@ def minStartValue(nums):
     for i in nums:
         rez += i
         rez_t.append(rez)
-    return 1 if min(rez_t) > 0 else abs(min(rez_t)) +1
+    return 1 if min(rez_t) > 0 else abs(min(rez_t)) + 1
 
 print(minStartValue([-3,2,-3,4,2]))
 print(minStartValue([1,-2,-3]))
