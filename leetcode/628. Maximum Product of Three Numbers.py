4308e283bbc853472f643983ce2f947ae04ce7f7
Alexander Radchenko <48209253+radchenko511@users.noreply.github.com>
2020-11-02 10:01:22 -0800

    Add 1413

--- leetcode/628. Maximum Product of Three Numbers.py
+++ leetcode/628. Maximum Product of Three Numbers.py
@@ -1,6 +1,7 @@
 import itertools
 
 def maximumProduct(nums):
+    print(sorted(nums))
     return max(sorted(nums)[-1]*sorted(nums)[-2]*sorted(nums)[-3], sorted(nums)[0]*sorted(nums)[1]*sorted(nums)[-1])
 
 print(maximumProduct([1,2,3,4]))
